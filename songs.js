153c8f3cb8c6bfb32faa399521e2979eab107793
guilhermelima <guilhermelimaruby@gmail.com>
2025-12-07 15:44:58 -0300

    v1.1

---
+++ songs.js
@@ -0,0 +1,785 @@
+const SONGS = [
+    { 
+        id: 1, 
+        title: "Vem, esta é a Hora", 
+        chord_text: `
+[D]                      [D4]  [D]
+Vem, esta é a hora da adoração
+ [A]                  [Em]  [G]
+Vem, dar a Ele teu coração
+ [D]                       [D4]    [D]
+Vem, assim como estás para adorar
+ [A]                      [Em]  [F#m]  [G]
+Vem, assim como estás diante do Pai
+ [D]
+Vem
+
+[Refrão]
+
+ [G]                   [D]
+Toda língua confessará o Senhor
+ [G]                   [D]
+Todo joelho se dobrará
+ [G]                  [Bm]
+Mas aquele que a Ti escolher
+     [Em]            [A4]  [A]
+O tesouro maior terá
+
+
+
+[Solo] [E]  [E4]  [E]
+       [B]  [F#m]  [A]
+       [E]  [E4]  [E]
+       [B]  [F#m]  [A]  [E][Tab - Solo]
+
+ [A]                   [E]
+Toda língua confessará o Senhor
+ [A]                   [E]
+Todo joelho se dobrará
+ [A]                  [C#m]
+Mas aquele que a Ti escolher
+     [F#m]           [B4]  [B]
+O tesouro maior terá
+
+ [E]                      [E4]  [E]
+Vem, esta é a hora da adoração
+ [B]                  [F#m]  [A]
+Vem, dar a Ele teu cora_ção
+ [E]                         [E4]  [E]
+Vem, assim como estás para adorar
+ [B]                         [F#m]   [A]
+Vem, assim como estás diante do Pai
+ [E]
+Vem, Vem, Vem, Vem, Vem
+`
+    },
+    { 
+        id: 2, 
+        title: "Ousado Amor", 
+        chord_text: `
+[C#m]                [B4]
+    Antes de eu falar
+             [A]
+Tu cantavas sobre mim
+[C#m]               [B4]
+    Tu tens sido tão
+        [A]
+Tão bom    pra mim
+[C#m]                   [B4]
+    Antes de eu respirar
+              [A]
+Sopraste Tua vida em mim
+[C#m]               [B4]
+    Tu tens sido tão
+        [A]
+Tão bom    pra mim
+
+[Refrão]
+
+       [C#m]          [B4]
+Oh, impressionante, infinito
+     [A]           [E]
+E ousado amor de Deus
+         [C#m]        [B4]
+Oh, que deixa as noventa e nove
+ [A]              [E]
+Só pra me encontrar
+               [C#m]            [B4]
+Não posso comprá-lo, nem merecê-lo
+         [A]          [E]
+Mesmo assim se entregou
+        [C#m]         [B4]
+Oh, impressionante, infinito
+     [A]           [E]
+E ousado amor de Deus
+
+( [C#m]  [B4]  [A]  [E] )
+( [C#m]  [B4]  [A]  [E] )
+
+[Segunda Parte]
+[C#m]             [B4]
+    Inimigo eu fui
+                [A]
+Mas Teu amor lutou por mim
+[C#m]               [B4]
+    Tu tens sido tão
+        [A]
+Tão bom    pra mim
+[C#m]              [B4]
+    Não tinha valor
+            [A]
+Mas tudo pagou por mim
+[C#m]               [B4]
+    Tu tens sido tão
+        [A]
+Tão bom    pra mim
+
+[Refrão]
+(Já apresentado acima)
+
+( [C#m]  [B4]  [A]  [E] )
+( [C#m]  [B4]  [A]  [E] )
+
+[Terceira Parte]
+[C#m]                   [B4]
+    Traz luz para as sombras
+           [A]                   [E]
+Escala montanhas, pra me encontrar
+[C#m]            [B4]
+    Derruba muralhas
+               [A]                  [E]
+Destrói as mentiras, pra me encontrar
+[C#m]                   [B4]
+    Traz luz para as sombras
+           [A]                   [E]
+Escala montanhas, pra me encontrar
+[C#m]            [B4]
+    Derruba muralhas
+               [A]                  [E]
+Destrói as mentiras, pra me encontrar[C#m]                   [B4]
+    Traz luz para as sombras
+           [A]                   [E]
+Escala montanhas, pra me encontrar[C#m]            [B4]
+    Derruba muralhas
+               [A]                  [E]
+Destrói as mentiras, pra me encontrar[C#m]                   [B4]
+    Traz luz para as sombras
+           [A]                   [E]
+Escala montanhas, pra me encontrar[C#m]            [B4]
+    Derruba muralhas
+               [A]                  [E]
+Destrói as mentiras, pra me encontrar
+`
+    },
+    { 
+        id: 3, 
+        title: "Perto quero estar", 
+        chord_text: `
+[A]                [D]
+  Perto quero estar
+[E]                 [A]
+  Junto aos teus pés
+[E]                [D]
+  Pois prazer maior não há
+[F#m]     [E]             [D]
+   Que me render e te adorar
+[A]                 [D]
+  Tudo que há em mim
+[E]               [A]
+  Quero te ofertar
+[E]               [D]
+  Mas, ainda é pouco eu sei
+[F#m]    [E]             [D]
+   Se comparado ao que ganhei
+
+
+[A]                 [D]
+  Não sou apenas servo
+      [E]          [A]
+Teu amigo me tornei
+
+( [D]  [E] )
+
+Refrão
+[A]     [E]    [D]
+  Te louvarei
+[A]        [E]
+  Não importam
+    [D]      [E]
+As circunstâncias
+[A]   [E]   [D]
+  Adorarei
+[F#m]           [E]    [A]
+   Somente a ti Jesus
+
+( [A]  [E] )
+
+[Primeira Parte]
+[A]                [D]
+  Perto quero estar
+[E]                 [A]
+  Junto aos teus pés
+[E]                [D]
+  Pois prazer maior não há
+[F#m]     [E]             [D]
+   Que me render e te adorar
+[A]                 [D]
+  Tudo que há em mim
+[E]               [A]
+  Quero te ofertar
+[E]               [D]
+  Mas, ainda é pouco eu sei
+[F#m]    [E]             [D]
+   Se comparado ao que ganhei
+
+[Pré-Refrão]
+[A]                 [D]
+  Não sou apenas servo
+      [E]          [A]
+Teu amigo me tornei
+
+( [D]  [E] )
+
+
+[A]     [E]    [D]
+  Te louvarei
+[A]        [E]
+  Não importam
+    [D]      [E]
+As circunstâncias
+[A]    [E]  [D]
+  Adorarei
+[D]            [E]    [A]
+  Somente a ti Jesus
+[A]     [E]    [D]
+  Te louvarei
+[A]        [E]
+  Não importam
+    [D]      [E]
+As circunstâncias
+[A]   [E]   [D]
+  Adorarei
+[D]            [E]    [A]
+  Somente a ti Jesus
+[A]      [E]   [D]
+  Te louvarei
+[A]        [E]
+  Não importam
+    [D]      [E]
+As circunstâncias
+[A]   [E]   [D]
+  Adorarei
+
+[D]            [E]  [A]
+  Somente a ti Jesus
+[D]            [E]  [A]
+  Somente a ti Jesus
+`
+    },
+    { 
+        id: 4, 
+        title: "Eu vou construir", 
+        chord_text: `
+[A]                 [D]
+   Digno desta canção só Tu és Senhor
+[A/C#]                  [D]
+      Digno do meu louvor só Tu és Senhor
+[A]                 [D]
+   Digno da minha vida Tu és Senhor
+           [A/C#]  [D]
+Oh eu sou Teu
+[A]                   [D]
+   Nome que é sobre todos é o Teu Jesus
+[A/C#]                [D]
+      Fonte da salvação só Tu és Jesus
+[A]                 [D]
+   Digno da minha vida Tu és Jesus
+           [A/C#]           [D]
+Oh eu sou Teu   oh eu sou Teu
+
+[Refrão]
+
+ [D]
+Santo
+           [Bm]
+És incomparável
+          [A/E]
+És inigualável
+             [F#m]
+Abre os meus olhos, Senhor
+
+ [D]
+Mostra quem Tu és
+  [Bm]          [A/E]
+E enche o meu coração do amor que faz
+ [F#m]
+Mudar o mundo
+
+( [A]  [E]  [D] )
+( [A]  [E]  [D] )
+
+[Primeira Parte]
+[A]                 [D]
+   Digno desta canção só Tu és Senhor
+[A/C#]                  [D]
+      Digno do meu louvor só Tu és Senhor
+[A]                 [D]
+   Digno da minha vida Tu és Senhor
+           [A/C#]  [D]
+Oh eu sou Teu
+[A]                   [D]
+   Nome que é sobre todos é o Teu Jesus
+[A/C#]                [D]
+      Fonte da salvação só Tu és Jesus
+[A]                 [D]
+   Digno da minha vida Tu és Jesus
+           [A/C#]           [D]
+Oh eu sou Teu   oh eu sou Teu
+
+[Refrão]
+
+ [D]
+Santo
+           [Bm]
+És incomparável
+          [A/E]
+És inigualável
+             [F#m]
+Abre os meus olhos, Senhor
+
+ [D]
+Mostra quem Tu és
+  [Bm]          [A/E]
+E enche o meu coração do amor que faz
+   [F#m]
+Mudar o mundo
+
+ [D]
+Santo
+           [Bm]
+És incomparável
+          [A/E]
+És inigualável
+             [F#m]
+Abre os meus olhos, Senhor
+
+ [D]
+Mostra quem Tu és
+  [Bm]          [A/E]
+E enche o meu coração do amor que faz
+   [F#m]
+Mudar o mundo
+
+[Ponte]
+
+( [D]  [E]  [F#m]  [A/C#] )
+( [D]  [E]  [F#m]  [A/C#] )
+ 
+[D]            [E]                 [F#m]
+Eu vou construir minha vida em Ti
+           [A/C#]
+Tu és meu fundamento
+[D]          [E]              [F#m]
+Eu vou confiar somente em Ti
+            [A/C#]
+Não vou ser abalado
+[D]            [E]                 [F#m]
+Eu vou construir minha vida em Ti
+           [A/C#]
+Tu és meu fundamento
+[D]          [E]              [F#m]
+Eu vou confiar somente em Ti
+            [A/C#]
+Não vou ser abalado
+[D]            [E]                 [F#m]
+Eu vou construir minha vida em Ti
+           [A/C#]
+Tu és meu fundamento
+[D]          [E]              [F#m]
+Eu vou confiar somente em Ti
+            [A/C#]
+Não vou ser abalado
+
+[Refrão]
+
+ [D]
+Santo
+           [Bm]
+És incomparável
+          [A/E]
+És inigualável
+             [F#m]
+Abre os meus olhos, Senhor
+
+ [D]
+Mostra quem Tu és
+  [Bm]          [A/E]
+E enche o meu coração do amor que faz
+ [F#m]
+Mudar o mundo
+`
+
+    }
+    ,
+    { 
+        id: 5, 
+        title: "A alegria está no coração", 
+        chord_text: `
+
+[C]            
+alegria está no coração     
+[F]                  [C]
+De quem já conhece a Jesus
+[C]             [Am]
+A verdadeira paz só tem aquele
+     [D]              [G]
+Que já conhece a Jesus
+
+
+[Refrão]
+[C]                   [C] 
+O sentimento mais precioso
+     [F]               [Fm]
+que vem do nosso Senhor
+[C] [Am]       [Dm]             [G]        [C]
+É o amor que só tem quem já conhece a Jesus
+
+[Ponte]
+         [C]                    
+Posso pisar numa tropa e saltar as muralhas
+    [F]
+Aleluiaaaa! Aleluiaaa!
+[C]                 [C7] 
+Ele é a rocha da minha salvação
+    [F]                [Fm]
+Com Ele não há mais condenação
+         [C]                [C] 
+Posso pisar numa tropa e saltar as muralhas
+    [F]           [C]
+Aleluiaaaa! Aleluiaaa!
+`
+
+    }
+    ,
+    { 
+        id: 6, 
+        title: "A benção", 
+        chord_text: `
+
+[G]                 [C]  
+  Que o Senhor te   abençoe
+[G]             [D]                [Em7]
+  E faça brilhar seu rosto em ti
+            [C]           [G]  
+Que conceda   Sua graça
+   [D]       [G]
+E te dê a paz
+
+[Refrão]
+[Em]  [C]     [G]     [D4]
+Am__ém, amém, amém
+[Em]  [C]     [G]     [D4]
+Am__ém, amém, amém
+
+[Segunda Parte]
+
+       [Em]
+Que a bênção se derrame
+     [C]
+Até mil gerações
+       [G]  
+Tua família e teus filhos
+      [D]
+E os filhos dos teus filhos
+       [Em]
+Que a bênção se derrame
+     [C]
+Até mil gerações
+       [G]  
+Tua família e teus filhos
+      [D]
+E os filhos dos teus filhos
+`
+
+    },
+    { 
+        id: 7, 
+        title: "A bondade de Deus", 
+        chord_text: `
+[D]
+Te amo, Deus
+      [G]          [D]
+Tua graça nunca falha
+          [Bm7]
+Todos os dias
+                  [A]
+Eu estou em Tuas mãos
+                   [Bm7]  [G]
+Desde quando me levanto
+        [D]        [Bm7]
+Até eu me deitar
+         [G]         [A]       [D]
+Eu cantarei da bondade de Deus
+
+[Refrão]
+[G]                  [D]
+  És fiel em todo tempo
+[G]                      [D]        [A]
+  Em todo tempo Tu és tão, tão bom
+[G]                      [Bm7]
+  Com todo fôlego que tenho
+         [G]         [A]       [D]
+Eu cantarei da bondade de Deus
+
+          [D]
+Tua doce voz
+         [G]              [D]
+Que me guia em meio ao fogo
+          [Bm7]
+Na escuridão
+        [G]           [A]
+Tua presença me conforta
+                [D]  [G]
+Sei que és meu Pai
+      [D]      [Bm7]
+Que amigo és
+      [G]     [A]       [D]
+Vivo na bondade de Deus
+
+
+[D/F#]                 [G]                
+     Tua bondade me seguirá
+    [A]          [D]
+Me seguirá Senhor
+[D/F#]                 [G]                
+     Tua bondade me seguirá
+    [A]          [D]
+Me seguirá Senhor
+
+       [D/F#]           [G]               
+Eu me rendo a Ti, te dou o meu ser
+    [A]           [Bm7]
+Entrego tudo a Ti
+[D/F#]                 [G]                
+     Tua bondade me seguirá
+    [A]          [D]
+Me seguirá Senhor
+`
+
+    },
+    { 
+        id: 8, 
+        title: "Abra os olhos", 
+        chord_text: `
+[E]
+Abra os olhos do meu coração
+[B/D#]
+Abra os olhos do meu coração
+          [C#m]  [A]            [E]
+Quero Te ver,       quero Te ver,   Senhor
+
+[Refrão]
+
+     [B]          [C#m]
+Exaltando e bem alto
+     [A]                    [B]
+Brilhando, a luz da Tua glória
+     [B]                [C#m]
+Derrame Seu amor e poder
+            [F#m]    [B]
+Pois Tu és Santo
+
+
+[E]
+Santo, Santo, Santo
+[B/D#]
+Santo, Santo, Santo
+[C#m]        [A]          [E]
+Santo, Santo, Santo
+            [E]
+Quero Te ver
+
+            [E]
+Eu quero Te ver
+          [B]
+Quero Te ver
+             [C#m]
+Eu quero Te tocar
+            [A]
+Quero Te abraçar
+          [F#m]  [B]
+Quero Te ver
+
+            [E]
+Eu quero Te ver
+          [B]
+Quero Te ver
+             [C#m]
+Eu quero Te tocar
+            [A]
+Quero Te abraçar
+          [F#m]  [B]
+Quero Te ver
+`
+
+    },
+    { 
+        id: 9, 
+        title: "Aleluia (Agnus Dei)", 
+        chord_text: `
+[C]   [F]  [C]
+Ale-lu-ia
+[C] [F]  
+Ale-lu-ia
+[F]                         [C]
+Poderoso é o Senhor nosso Deus
+[C]   [F]  [C]
+Ale-lu-ia
+[C]   [F]  
+Ale-lu-ia
+[F]                         [C]
+Poderoso é o Senhor nosso Deus
+[C]   [F]  
+Ale-lu-ia 
+
+[Refrão]
+[C]   [C]
+San-n-to, Santo
+      [C]             [G]
+É Senhor Deus poderoso
+            [F]
+Digno de Louvor
+            [F]
+Digno de Louvor
+        [C]      [C]
+Tú és Santo, Santo
+      [C]             [G]
+É Senhor Deus poderoso
+            [F]
+Digno de Louvor
+            [F]
+Digno de Louvor
+[C]
+Amem
+`
+
+    },
+    { 
+        id: 10, 
+        title: "Existe um rio", 
+        chord_text: `
+[D]                  [Em]
+Existe um rio, Senhor
+  [D]                      [Em]
+Que flui do Teu grande amor
+[G]                     [D] 
+Águas que correm do trono
+[G]                     [A] 
+Águas que curam, que limpam
+
+ [D]                 [Em]
+Por onde o rio passar
+ [Bm7]              [A] 
+Tudo vai transformar
+      [G]               [D] 
+Pois leva a vida do próprio Deus
+        [G]     [Em] 
+E este rio está 
+ [A]          [G/B]  [A]
+Neste lugar
+
+[Refrão] 
+
+  [D]                 [G]       [D]  
+Quero beber do Teu rio, Senhor
+   [G]         [D]            [Em]       [A]
+Sacia minha sede, lava o meu interior
+     [Bm7]               [G]     [A]
+Eu quero fluir em Tuas águas
+     [Bm7]    [F#m7]       [G]
+Eu quero beber da Tua fonte
+                [A] 
+Fonte de águas vivas
+         [G]         [D]
+Tu és a fonte, Senhor
+
+
+
+                   [D]  
+Tu és a fonte, Senhor
+
+Tu és o rio, Senhor
+                   [Bm]  [A]
+Tu és a fonte, Senhor
+                 [G]  [A]
+Tu és o rio, Senhor
+                   [D]  
+Tu és a fonte, Senhor
+
+Tu és o rio, Senhor
+                   [Bm]  [A]
+Tu és a fonte, Senhor
+                 [G]  [A]
+Tu és o rio, Senhor
+                 [D] 
+Tu és o rio, Senhor
+`
+
+    },
+    { 
+        id: 11, 
+        title: "Ainda que a figueira", 
+        chord_text: `
+[F#m]            [E]
+   Tu és a minha porção
+[Bm7]          [D]
+    Tu és a minha herança
+[F#m]         [E]
+   Tu és o meu socorro
+     [Bm7]            [D]
+Nos dias de tribulação
+[A]                 [E]
+  Mesmo que meus pais me deixem
+[Bm7]             [D]
+    Mesmo que amigos me traiam
+[A]                      [E]
+  Eu sei que em Seus braços
+      [Bm7]         [D]
+Eu encontro salvação
+
+[Refrão]
+
+ [A]                   
+Ainda que a figueira 
+ [E]
+Não floresça
+ [Bm7]                
+Ainda que a videira 
+     [D]
+Não dê o seu fruto
+ [A]                     [E]
+Mesmo que não haja alimento 
+
+Nos campos
+        [G]           [D]
+Eu me alegrarei em Ti
+`
+    },
+    { 
+        id: 13, 
+        title: "Alegria - Valadão", 
+        chord_text: `
+[C]     [Am]        [Em] 
+TU ME AMAS COMO SOU 
+            [G]          [C] 
+MAS NÁO ME DEIXAS COMO ESTOU 
+            [Am]            [Em] 
+SEGUIR TEUS PASSOS SEMPRE VOU  
+          [G]             [C] 
+RUMO AO ENCONTRO DO TEU AMOR 
+      [Am]                 [Em] 
+O TEU AMOR É MAIOR QUE A VIDA 
+         [G]        [C] 
+ME TRAZ PAZ E ALEGRIA 
+          [F]           [G]    [Am] 
+COM TEU PERFUME QUERO ME ENCHER 
+             [G]               [C] 
+E A CADA INSTANTE MAIS TE CONHECER 
+
+       [Dm]             [C/E]            [F]     [G] 
+TUA VERDADE É A LUZ QUE ME CONDUZ PARA O CÉU 
+       [Dm]                 [C/E]         [F]    [G] 
+TUA PALAVRA É O REMÉDIO QUE CURA TODO MEU SER 
+
+
+       [F]             [C/E] 
+TUA ALEGRIA EM MINHA VIDA 
+[Dm]                       [Gsus]  [G] 
+ME FORTALECE A CADA NOVO DIA 
+       [F]            [C/E] 
+TUA ALEGRIA É MINHA VIDA 
+            [Dm]      [G] 
+RECEBO DELA HOJE, AGORA
+`
+    },
+].sort((a, b) => a.title.localeCompare(b.title));
